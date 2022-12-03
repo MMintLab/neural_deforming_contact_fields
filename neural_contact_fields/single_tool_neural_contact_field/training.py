dbd6f55b52f296c1f7712e978ec8bbc64fea43c7
Mark Van der Merwe <markvdm@umich.edu>
2022-12-03 15:45:28 -0500

    Setup the training loop structure.

--- neural_contact_fields/single_tool_neural_contact_field/training.py
+++ neural_contact_fields/single_tool_neural_contact_field/training.py
@@ -1,203 +1,189 @@
+import mmint_utils
 import torch
 from neural_contact_fields.training import BaseTrainer
 import torch.nn.functional as F
 import os
-import mmint_utils
 from neural_contact_fields.utils import model_utils
 from tensorboardX import SummaryWriter
-import torch.optim as optim
+from torch import optim
+from torch.utils.data import Dataset
 
 
 class Trainer(BaseTrainer):
 
-    def __init__(self, cfg, model, device=None):
+    def load_pretrained_model(self, pretrain_file: str, freeze_object_module_weights=False):
         """
-        Args:
-        - model (nn.Module): model
-        - optimizer (optimizer): pytorch optimizer
-        - logger (tensorboardX.SummaryWriter): logger for tensorboard
-        - vis_dir (str): vis directory
-        - device (device): pytorch device
+        Load pretrained model weights. Optionally freeze object module weights.
+
+        Note: assumes object_module defined in model.
         """
-        self.model = model
-        self.device = device
+        print("Loading pretrained model weights from local file: %s" % pretrain_file)
+        pretrain_state_dict = torch.load(pretrain_file, map_location='cpu')
 
-        # Shorthands:
-        self.out_dir = cfg['training']['out_dir']
-        self.lr = cfg['training']['learning_rate']
-        self.print_every = cfg['training']['print_every']
-        self.max_epochs = cfg['training']['epochs']
-        self.loss_weights = cfg['training']['loss_weights']
-        self.epoch_it = 0
-        self.it = 0
+        # Here, we only load object module weights.
+        object_module_keys = [key for key in pretrain_state_dict["model"].keys() if "object_module" in key]
+        object_module_dict = {k: pretrain_state_dict["model"][k] for k in object_module_keys}
+        self.model.load_state_dict(object_module_dict, strict=False)
 
-        # Output + vis directory
-        if not os.path.exists(self.out_dir):
-            os.makedirs(self.out_dir)
-        self.logger = SummaryWriter(os.path.join(self.out_dir, 'logs'))
-
-        # Dump config to output directory.
-        mmint_utils.dump_cfg(os.path.join(self.out_dir, 'config.yaml'), cfg)
-
-        # Get optimizer (TODO: Parameterize?)
-        self.optimizer = optim.Adam(model.parameters(), lr=self.lr)
-
-        # Load pretrained model, if appropriate.
-        self.pretrain_file = os.path.join(self.out_dir, cfg['training']['pretrain_file'])
-        self.load_pretrained_model(cfg['training']['freeze_pretrain_weights'])
-
-        # Load model + optimizer if a partially trained copy of it exists.
-        self.load_partial_train_model()
-
-    def load_partial_train_model(self):
-        model_dict = {
-            'model': self.model,
-            'optimizer': self.optimizer,
-        }
-        model_file = os.path.join(self.out_dir, 'model.pt')
-        load_dict = model_utils.load_model(model_dict, model_file)
-        self.epoch_it = load_dict.get('epoch_it', -1)
-        self.it = load_dict.get('it', -1)
+        # Optionally, we can freeze the pretrained weights.
+        if freeze_object_module_weights:
+            for param in self.model.object_module.parameters():
+                param.requires_grad = False
 
     ##########################################################################
     #  Pretraining loop                                                      #
     ##########################################################################
 
-    def load_pretrained_model(self, freeze_pretrain_weights=False):
-        # Load model pretrain weights, if exists.
-        if self.pretrain_file is not None:
-            print("Loading pretrained model weights from local file: %s" % self.pretrain_file)
-            pretrain_state_dict = torch.load(self.pretrain_file, map_location='cpu')
+    def pretrain(self, pretrain_dataset: Dataset):
+        # Shorthands:
+        out_dir = self.cfg['pretraining']['out_dir']
+        lr = self.cfg['pretraining']['learning_rate']
+        max_epochs = self.cfg['pretraining']['epochs']
+        self.pretrain_loss_weights = self.cfg['pretraining']['loss_weights']  # TODO: Better way to set this?
+        epoch_it = 0
+        it = 0
 
-            # Here, we only load object module weights.
-            object_module_keys = [key for key in pretrain_state_dict["model"].keys() if "object_module" in key]
-            object_module_dict = {k: pretrain_state_dict["model"][k] for k in object_module_keys}
-            self.model.load_state_dict(object_module_dict, strict=False)
+        # Output + vis directory
+        if not os.path.exists(out_dir):
+            os.makedirs(out_dir)
+        logger = SummaryWriter(os.path.join(out_dir, 'logs'))
 
-            # Optionally, we can freeze the pretrained weights.
-            if freeze_pretrain_weights:
-                for param in self.model.object_module.parameters():
-                    param.requires_grad = False
+        # Dump config to output directory.
+        mmint_utils.dump_cfg(os.path.join(out_dir, 'config.yaml'), self.cfg)
 
-    def pretrain(self, pretrain_dataset):
-        raise NotImplementedError()
+        # Get optimizer (TODO: Parameterize?)
+        optimizer = optim.Adam(self.model.parameters(), lr=lr)
 
-    ##########################################################################
-    #  Main training loop                                                    #
-    ##########################################################################
+        # Load model + optimizer if a partially trained copy of it exists.
+        self.load_partial_train_model(optimizer, out_dir, "pretrain_model.pt")
 
-    def train(self, train_dataset):
         # Training loop
         while True:
-            self.epoch_it += 1
+            epoch_it += 1
 
-            if self.epoch_it > self.max_epochs:
+            if epoch_it > max_epochs:
                 print("Backing up and stopping training. Reached max epochs.")
                 save_dict = {
                     'model': self.model.state_dict(),
-                    'optimizer': self.optimizer.state_dict(),
-                    'epoch_it': self.epoch_it,
-                    'it': self.it,
+                    'optimizer': optimizer.state_dict(),
+                    'epoch_it': epoch_it,
+                    'it': it,
                 }
-                torch.save(save_dict, os.path.join(self.out_dir, 'model.pt'))
+                torch.save(save_dict, os.path.join(out_dir, 'pretrain_model.pt'))
                 break
 
             loss = None
-            for trial_idx in range(train_dataset.num_trials):
-                self.it += 1
 
-                batch = train_dataset.get_all_points_for_trial_batch(-1, trial_idx)
-                loss = self.train_step(batch, self.it)
+            for trial_idx in range(len(pretrain_dataset)):
+                it += 1
+
+                # For this training, we use just a single example per run.
+                batch = pretrain_dataset[trial_idx]
+                loss = self.train_step(batch, it, optimizer, logger, self.compute_pretrain_loss)
 
             print('[Epoch %02d] it=%03d, loss=%.4f'
-                  % (self.epoch_it, self.it, loss))
+                  % (epoch_it, it, loss))
 
-            # Backup.
-            save_dict = {
-                'model': self.model.state_dict(),
-                'optimizer': self.optimizer.state_dict(),
-                'epoch_it': self.epoch_it,
-                'it': self.it,
-            }
-            torch.save(save_dict, os.path.join(self.out_dir, 'model.pt'))
+        # Backup.
+        save_dict = {
+            'model': self.model.state_dict(),
+            'optimizer': optimizer.state_dict(),
+            'epoch_it': epoch_it,
+            'it': it,
+        }
+        torch.save(save_dict, os.path.join(out_dir, 'pretrain_model.pt'))
 
-    def train_step(self, data, it):
-        """
-        Perform training step.
-
-        Args:
-        - data (dict): batch data dict
-        - it (int): training iter
-        """
-        self.model.train()
-        self.optimizer.zero_grad()
-        loss_dict, _ = self.compute_loss(data, it)
-
-        for k, v in loss_dict.items():
-            self.logger.add_scalar(k, v, it)
-
-        loss = loss_dict['loss']
-        loss.backward()
-        self.optimizer.step()
-
-        return loss.item()
-
-    def compute_loss(self, data, it):
+    def compute_pretrain_loss(self, data, it) -> (dict, dict):
         """
         Get loss over provided batch of data.
 
         Args:
         - data (dict): data dictionary
         """
-        device = self.device
         loss_dict = dict()
         out_dict = dict()
 
-        # Send data to torch/device.
-        trial_idx = data["trial_idx"].long().to(device)
-        query_point = data["query_point"].float().to(device)
-        sdf = data["sdf"].float().to(device)
-        surface_query_points = sdf == 0.0
-        in_contact = data["in_contact"].to(device) > 0.5
-        in_contact_float = torch.clone(in_contact).float()
-        force = data["force"].float().to(device)
+        loss_dict["loss"] = 0.0
 
-        pred_dict = self.model.forward(trial_idx, query_point)
+        return loss_dict, out_dict
 
-        # Apply losses.
+    ##########################################################################
+    #  Main training loop                                                    #
+    ##########################################################################
 
-        # Apply L2 loss on trial embeddings.
-        embed_loss = torch.linalg.norm(pred_dict["z"]).mean()
-        loss_dict["embed_loss"] = embed_loss  # TODO: This is not being used.
+    def train(self, train_dataset: Dataset):
+        # Shorthands:
+        out_dir = self.cfg['training']['out_dir']
+        lr = self.cfg['training']['learning_rate']
+        max_epochs = self.cfg['training']['epochs']
+        self.train_loss_weights = self.cfg['training']['loss_weights']  # TODO: Better way to set this?
+        epoch_it = 0
+        it = 0
 
-        # We apply the SDF loss to every point in space.
-        sdf_loss = F.l1_loss(pred_dict["sdf"], sdf, reduction="mean")
-        loss_dict["sdf_loss"] = sdf_loss
+        # Output + vis directory
+        if not os.path.exists(out_dir):
+            os.makedirs(out_dir)
+        logger = SummaryWriter(os.path.join(out_dir, 'logs'))
 
-        # Apply L2 loss on deformation.
-        def_loss = torch.linalg.norm(pred_dict["delta_coords"]).mean()
-        loss_dict["def_loss"] = def_loss
+        # Dump config to output directory.
+        mmint_utils.dump_cfg(os.path.join(out_dir, 'config.yaml'), self.cfg)
 
-        # Next, for all points *on the surface* we predict the contact probability.
-        if surface_query_points.sum() > 0:
-            contact_loss = F.binary_cross_entropy_with_logits(pred_dict["contact_logits"][surface_query_points],
-                                                              in_contact_float[surface_query_points], reduction="mean")
-            loss_dict["contact_loss"] = contact_loss
-        else:
-            loss_dict["contact_loss"] = 0.0
+        # Get optimizer (TODO: Parameterize?)
+        optimizer = optim.Adam(self.model.parameters(), lr=lr)
 
-        # Finally, for all points *in contact* we predict the contact forces.
-        if in_contact.sum() > 0:
-            force_loss = F.mse_loss(pred_dict["contact_force"][in_contact], force[in_contact], reduction="mean")
-            loss_dict["force_loss"] = force_loss
-        else:
-            loss_dict["force_loss"] = 0.0
+        # Load pretrained model, if appropriate.
+        pretrain_file = os.path.join(out_dir, self.cfg['training']['pretrain_file'])
+        self.load_pretrained_model(pretrain_file, self.cfg['training']['freeze_pretrain_weights'])
 
-        # Combined losses.
-        loss = (self.loss_weights["sdf_loss"] * sdf_loss) + \
-               (self.loss_weights["def_loss"] * def_loss) + \
-               (self.loss_weights["contact_loss"] * loss_dict["contact_loss"]) + \
-               (self.loss_weights["force_loss"] * loss_dict["force_loss"])
-        loss_dict["loss"] = loss
+        # Load model + optimizer if a partially trained copy of it exists.
+        self.load_partial_train_model(optimizer, out_dir, "model.pt")
+
+        # Training loop
+        while True:
+            epoch_it += 1
+
+            if epoch_it > max_epochs:
+                print("Backing up and stopping training. Reached max epochs.")
+                save_dict = {
+                    'model': self.model.state_dict(),
+                    'optimizer': optimizer.state_dict(),
+                    'epoch_it': epoch_it,
+                    'it': it,
+                }
+                torch.save(save_dict, os.path.join(out_dir, 'model.pt'))
+                break
+
+            loss = None
+
+            for trial_idx in range(len(train_dataset)):
+                it += 1
+
+                # For this training, we use just a single example per run.
+                batch = train_dataset[trial_idx]
+                loss = self.train_step(batch, it, optimizer, logger, self.compute_train_loss)
+
+            print('[Epoch %02d] it=%03d, loss=%.4f'
+                  % (epoch_it, it, loss))
+
+        # Backup.
+        save_dict = {
+            'model': self.model.state_dict(),
+            'optimizer': optimizer.state_dict(),
+            'epoch_it': epoch_it,
+            'it': it,
+        }
+        torch.save(save_dict, os.path.join(out_dir, 'model.pt'))
+
+    def compute_train_loss(self, data, it) -> (dict, dict):
+        """
+        Get loss over provided batch of data.
+
+        Args:
+        - data (dict): data dictionary
+        """
+        loss_dict = dict()
+        out_dict = dict()
+
+        loss_dict["loss"] = 0.0
 
         return loss_dict, out_dict
--- neural_contact_fields/training.py
+++ neural_contact_fields/training.py
@@ -1,15 +1,68 @@
-from collections import defaultdict
+import os
 
-import numpy as np
-from tqdm import tqdm
+from neural_contact_fields.utils import model_utils
+from torch.utils.data import Dataset
 
 
 class BaseTrainer(object):
     """
     Base trainer class.
-
-    Modified from: https://github.com/autonomousvision/occupancy_networks/blob/master/im2mesh/training.py
     """
+
+    def __init__(self, cfg, model, device=None):
+        """
+        Args:
+        - cfg (dict): trainer configuration
+        - model (nn.Module): model
+        - device (device): pytorch device
+        """
+        self.cfg = cfg
+        self.model = model
+        self.device = device
+
+    def load_partial_train_model(self, optimizer, out_dir, partial_model_fn):
+        """
+        Loads a partially trained model from the given out/fn and attempts to load optimizer state.
+
+        Args:
+        - optimizer (torch.optim.Optimizer): optimizer being used in training
+        - out_dir (str): out directory where files are being saved
+        - partial_model_fn (str): file where partially trained model is
+        """
+        model_dict = {
+            'model': self.model,
+            'optimizer': optimizer,
+        }
+        model_file = os.path.join(out_dir, partial_model_fn)
+        load_dict = model_utils.load_model(model_dict, model_file)
+        epoch_it = load_dict.get('epoch_it', -1)
+        it = load_dict.get('it', -1)
+        return epoch_it, it
+
+    def train_step(self, data, it, optimizer, logger, compute_loss_fn):
+        """
+        Perform training step. This wraps up the gradient calculation for convenience.
+
+        Args:
+        - data (dict): batch data dict
+        - it (int): training iter
+        - optimizer (torch.optim.Optimizer): optimizer used for training
+        - logger: tensorboard logger being used.
+        - compute_loss_fn (callable): loss function to call. Should return a loss dictionary with main
+          loss at key "loss" and an out_dict
+        """
+        self.model.train()
+        optimizer.zero_grad()
+        loss_dict, _ = compute_loss_fn(data, it)
+
+        for k, v in loss_dict.items():
+            logger.add_scalar(k, v, it)
+
+        loss = loss_dict['loss']
+        loss.backward()
+        optimizer.step()
+
+        return loss.item()
 
     def pretrain(self, *args, **kwargs):
         """
