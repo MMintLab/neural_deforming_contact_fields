aac2747636f502df7b822f9367328ed08b844033
Mark Van der Merwe <markvdm@umich.edu>
2023-04-07 22:06:16 -0400

    Fix up generation tests for noise.

--- scripts/bash/gen_inf_params_noise.sh
+++ scripts/bash/gen_inf_params_noise.sh
@@ -30,23 +30,23 @@ OUT_BASE="out/experiments/primitives/model_v2/eval_inference_noise_gen"
 #python scripts/eval_inference.py $MODEL_CFG -d $DATA_CFG -m test -o $OUT_BASE/iter_limit/100/ --gen_args "{"iter_limit": 100}" -n 3
 #python scripts/eval_inference.py $MODEL_CFG -d $DATA_CFG -m test -o $OUT_BASE/iter_limit/300/ --gen_args "{"iter_limit": 300}" -n 3
 
-python scripts/generate.py $MODEL_CFG -d $DATA_CFG -m test -o $OUT_BASE/random/ --gen_args "{"iter_limit": 0}"
+# python scripts/generate.py $MODEL_CFG -d $DATA_CFG -m test -o $OUT_BASE/random/ --gen_args "{"iter_limit": 0}"
 #
 # python scripts/generate.py $MODEL_CFG -d $DATA_CFG -m test -o $OUT_BASE/embed_weight_std/0.0001/ --gen_args "{"embed_weight": 0.0001}"
 #python scripts/generate.py $MODEL_CFG -d $DATA_CFG -m test -o $OUT_BASE/embed_weight/0/ --gen_args "{"embed_weight": 0.0}"
 # python scripts/generate.py $MODEL_CFG -d $DATA_CFG -m test -o $OUT_BASE/embed_weight/0_1000/ --gen_args "{"embed_weight": 0.0, "iter_limit": 1000}"
 
-python scripts/generate.py $MODEL_CFG -d $DATA_CFG -m test -o $OUT_BASE/embed_iter/0.001_50/ --gen_args "{"embed_weight": 0.001, "iter_limit": 50}"
-python scripts/generate.py $MODEL_CFG -d $DATA_CFG -m test -o $OUT_BASE/embed_iter/0.001_100/ --gen_args "{"embed_weight": 0.001, "iter_limit": 100}"
+#python scripts/generate.py $MODEL_CFG -d $DATA_CFG -m test -o $OUT_BASE/embed_iter/0.001_50/ --gen_args "{"embed_weight": 0.001, "iter_limit": 50}"
+#python scripts/generate.py $MODEL_CFG -d $DATA_CFG -m test -o $OUT_BASE/embed_iter/0.001_100/ --gen_args "{"embed_weight": 0.001, "iter_limit": 100}"
 #python scripts/generate.py $MODEL_CFG -d $DATA_CFG -m test -o $OUT_BASE/embed_iter/0.001_150/ --gen_args "{"embed_weight": 0.001, "iter_limit": 150}"
 #
-python scripts/generate.py $MODEL_CFG -d $DATA_CFG -m test -o $OUT_BASE/embed_iter/0.0001_50/ --gen_args "{"embed_weight": 0.0001, "iter_limit": 50}"
-python scripts/generate.py $MODEL_CFG -d $DATA_CFG -m test -o $OUT_BASE/embed_iter/0.0001_100/ --gen_args "{"embed_weight": 0.0001, "iter_limit": 100}"
+#python scripts/generate.py $MODEL_CFG -d $DATA_CFG -m test -o $OUT_BASE/embed_iter/0.0001_50/ --gen_args "{"embed_weight": 0.0001, "iter_limit": 50}"
+#python scripts/generate.py $MODEL_CFG -d $DATA_CFG -m test -o $OUT_BASE/embed_iter/0.0001_100/ --gen_args "{"embed_weight": 0.0001, "iter_limit": 100}"
 #python scripts/generate.py $MODEL_CFG -d $DATA_CFG -m test -o $OUT_BASE/embed_iter/0.0001_150/ --gen_args "{"embed_weight": 0.0001, "iter_limit": 150}"
 
 #python scripts/generate.py $MODEL_CFG -d $DATA_CFG -m test -o $OUT_BASE/embed_conv/0.001_0.0001/ --gen_args "{"embed_weight": 0.001, "conv_eps": 0.0001}"
 
-# python scripts/eval_results.py $DATA_CFG $OUT_BASE/random/ -m test
+#python scripts/eval_results.py $DATA_CFG $OUT_BASE/random/ -m test
 #
 #python scripts/eval_results.py $DATA_CFG $OUT_BASE/embed_weight/1.0/ -m test
 #python scripts/eval_results.py $DATA_CFG $OUT_BASE/embed_weight/0.1/ -m test
@@ -97,4 +97,4 @@ python scripts/generate.py $MODEL_CFG -d $DATA_CFG -m test -o $OUT_BASE/embed_it
 
 # python scripts/plot/plot_inf_params_single.py $OUT_BASE/random/ $OUT_BASE/embed_weight/0.0001/ $OUT_BASE/embed_weight/0/ $OUT_BASE/embed_weight/0_1000/ $OUT_BASE/embed_conv/0.001_0.0001/ $OUT_BASE/embed_weight_std/std_0.0001/ $OUT_BASE/gen_res.csv
 
-python scripts/plot/plot_inf_params_single.py $OUT_BASE/random/ $OUT_BASE/embed_iter/0.001_50/ $OUT_BASE/embed_iter/0.001_100/ $OUT_BASE/embed_iter/0.001_150/ $OUT_BASE/embed_iter/0.0001_50/ $OUT_BASE/embed_iter/0.0001_100/ $OUT_BASE/embed_iter/0.0001_150/ $OUT_BASE/embed_iter.csv
+python scripts/plot/plot_inf_params_single.py $OUT_BASE/random/ $OUT_BASE/embed_iter/0.001_50/ $OUT_BASE/embed_iter/0.001_100/ $OUT_BASE/embed_iter/0.0001_50/ $OUT_BASE/embed_iter/0.0001_100/ $OUT_BASE/embed_iter.csv
