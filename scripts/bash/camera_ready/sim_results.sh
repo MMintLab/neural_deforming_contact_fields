12e6a454cb809f1616601a7cea5005d0cbad0bf3
Mark Van der Merwe <markvdm@umich.edu>
2023-05-21 21:24:07 -0400

    P2

--- scripts/bash/camera_ready/sim_results.sh
+++ scripts/bash/camera_ready/sim_results.sh
@@ -4,4 +4,6 @@
 
 # python scripts/generate.py cfg/primitives/camera_ready/model_v1.yaml -d cfg/primitives/camera_ready/dataset/test_v1.yaml -m test -o out/experiments/camera_ready/sim/test_v1/model_v1_v3/ --gen_args "{"iter_limit": 100, "embed_weight": 0.001, "contact_threshold": 0.2, "num_latent": 4}"
 
-python scripts/generate.py cfg/primitives/camera_ready/model_v3.yaml -d cfg/primitives/camera_ready/dataset/test_v1.yaml -m test -o out/experiments/camera_ready/sim/test_v1/model_v3/ --gen_args "{"iter_limit": 100, "embed_weight": 0.001, "contact_threshold": 0.2}"
+# python scripts/generate.py cfg/primitives/camera_ready/model_v3.yaml -d cfg/primitives/camera_ready/dataset/test_v1.yaml -m test -o out/experiments/camera_ready/sim/test_v1/model_v3/ --gen_args "{"iter_limit": 100, "embed_weight": 0.001, "contact_threshold": 0.2}"
+
+python scripts/generate.py cfg/primitives/camera_ready/model_v3.yaml -d cfg/primitives/camera_ready/dataset/test_v1_p2.yaml -m test -o out/experiments/camera_ready/sim/test_v1/model_v3_p2/ --gen_args "{"iter_limit": 100, "embed_weight": 0.001, "contact_threshold": 0.2}"
